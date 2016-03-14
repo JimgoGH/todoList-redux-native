fc986a791a26b9cc255d859390a944b196653c91
Jimgo Wang <jimgo@sina.com>
2016-03-14 12:23:19 +0800

    Update index.android.js

--- index.android.js
+++ index.android.js
@@ -14,15 +14,6 @@ import React, {
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-/*
-import BigButton2 from './common/components/BigButton2';
-import Navigator1 from './common/components/Navigator1';
-import IptC1 from './common/components/InputChange1';
-import VBAM1 from './common/components/ViewByArrayMap1';
-import MT1 from './common/components/Moutons1';
-*/
-//import TodoList1 from './redux/todoList1/todoList1';
-
 import App from './redux/todoList1/containers/App';
 import todoApp from './redux/todoList1/reducers/reducer';
 
